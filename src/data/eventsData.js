7c8c9966f884bdf4aa47ff9dd0848c212052c220
Explora Manaus <exploramanaus@protonmail.com>
2024-11-24 19:58:20 -0800

    alteracao nos eventos e locais

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -1,47 +1,91 @@
 // src/data/eventsData.js
 const eventsData = [
     {
-        name: 'Festival de Música Amazônica',
-        description: 'Um evento musical com apresentações de artistas locais e regionais.',
+        name: 'Show Da Banda Sepultura',
+        description: 'Sepultura apresentera seu show Celebrating The Through Death trazendo vários clássicos da banda, não perca essa experiencia unica!! ',
         category: 'Cultura e Entretenimento',
-        date: '2024-11-15',
-        location: 'Teatro Amazonas',
-        rating: 3.5,
-        reviews: 40,
-        coordinates: [-60.021731, -3.100012]
+        date: '05/12/2024',
+        location: 'Studio 5',
+        price:'130,00 - 340,00',
+        pageLink:'https://www.bilheteriadigital.com/sepultura-manaus-05-de-dezembro',
+        time: '18:00',
+        coordinates: [-59.983331070509465, -3.124321688768857]
+        
     },
     {
-        name: 'Corrida Internacional de Manaus',
+        name: 'Corrida Amazônica',
         description: 'Evento esportivo anual com atletas nacionais e internacionais.',
         category: 'Esportes',
-        date: '2024-12-05',
-        location: 'Ponta Negra',
-        coordinates: [-60.037885, -3.105852]
+        date: '31/12/2024',
+        pageLink:'https://www.ticketsports.com.br/e/corrida-amazonica-70174',
+        location: 'UFAM - Em frente ao Estacionamento da UFAM-FACED',
+        coordinates: [-59.97652343390843, -3.1001546116298977]
     },
     {
-        name: 'Feira de Startups e Tecnologia',
-        description: 'Oportunidade para empreendedores apresentarem inovações tecnológicas.',
+        name: 'Inovatech',
+        description: 'Oportunidade para alunos de gradução apresentarem inovações tecnológicas.',
         category: 'Tecnologia e Inovação',
-        date: '2024-10-22',
-        location: 'Centro de Convenções Vasco Vasques',
-        coordinates: [-60.035690, -3.101915]
+        date: '25/11/2024',
+        time: '8:30 - 11:30 / 18:30 - 21:30',
+        location: 'Manaus Plaza Shopping',
+        coordinates: [-60.02393800092801, -3.097380193553489] 
     },
     {
-        name: 'Workshop de Empreendedorismo',
-        description: 'Palestras e workshops focados em negócios e educação.',
+        name: '7º Congresso Amazônico de Iniciação Científica',
+        description: 'O evento tem como objetivo central estimular a produção científica e incentivar a reflexão sobre o papel da ciência na resolução dos desafios socioambientais que afetam a maior floresta tropical do mundo.',
         category: 'Negócios e Educação',
-        date: '2024-11-01',
-        location: 'Sebrae Amazonas',
-        coordinates: [-60.019831, -3.112789]
+        date: '28/11/2024',
+        pageLink:'https://www.sympla.com.br/evento/7-congresso-amazonico-de-iniciacao-cientifica/2682585',
+        location: 'Faculdade La Salle',
+        coordinates: [-60.03976407790977,  -3.0878429215870438]
     },
     {
-        name: 'Festival Gastronômico de Manaus',
-        description: 'Culinária local com chefs renomados e pratos típicos da região.',
-        category: 'Gastronomia e Lazer',
-        date: '2024-11-10',
-        location: 'Praça da Saudade',
-        coordinates: [-60.025555, -3.116948]
-    }
+        name: 'Natalien',
+        description: 'Festival de música eletrônica com uma estrutura surpreendente que ira receber grandes artistas da musica eletrônica tanto mundialemnte quanto nossos destaques regionais ',
+        category: 'Cultura e Entretenimento',
+        date: '25/12/2024',
+        price:'30,00 - 160,00',
+        pageLink:'https://www.sympla.com.br/evento/natalien-10-anos/2659470?referrer=www.google.com',
+        location: 'litoranea77',
+        coordinates: [-60.0984107423293, -3.0243757791163715]
+    },
+
+    {
+        name: 'Oficina G3 - DDG Reloaded Tour',
+        description: 'A renomada banda de rock cristão Oficina G3 apresenta sua turnê "DDG Reloaded" em Manaus.',
+        category: 'Cultura e Entretenimento',
+        date: '30/11/2024',
+        location: 'Igreja Batista Maranatha',
+        price: '100,00 - 200,00',
+        pageLink: 'https://www.eventoon.com.br/evento/oficina-g3-ddg-reloaded-tour-manaus',
+        time: '19:00',
+        coordinates: [-59.9386, -3.1019]
+    },
+
+    {
+        name: 'Aloha Réveillon 2025',
+        description: 'Celebração de Ano Novo com temática havaiana, música ao vivo e queima de fogos.',
+        category: 'Cultura e Entretenimento',
+        date: '31/12/2024',
+        location: 'PSG Arena Paris',
+        price: '200,00 - 400,00',
+        pageLink: 'https://www.eventoon.com.br/evento/aloha-reveillon-2025-em-manaus',
+        time: '22:00',
+        coordinates: [-60.0257, -3.0833]
+    },
+
+    {
+        name: 'Atemporal Manaus',
+        description: 'Evento musical que reúne diversos artistas em uma noite de celebração da música atemporal.',
+        category: 'Cultura e Entretenimento',
+        date: '13/12/2024',
+        location: 'Podium da Arena da Amazônia',
+        price: '250,00 - 400,00',
+        pageLink: 'https://www.eventoon.com.br/evento/atemporal-manaus',
+        time: '20:00',
+        coordinates: [-60.0257, -3.0833]
+    },
+    
 ];
 
 export default eventsData;
